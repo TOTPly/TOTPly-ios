4b421a8f95eab2fedf140a94474b69adc58e4598
Matthew <mateogrigio@gmail.com>
2026-02-23 15:43:29 +0300

    project setup

---
+++ TOTPly-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TOTPly-ios
+//
+//  Created by Matthew on 23.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
