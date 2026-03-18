650edb27caa7069e8676be87a1015dbf2f50c976
Matthew <mateogrigio@gmail.com>
2026-03-18 12:44:26 +0300

    live timer; works with scroll

--- Sources/Modules/Dashboard/Models/TOTPCellModel.swift
+++ Sources/Modules/Dashboard/Models/TOTPCellModel.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+// де-факто view state ячейки таблицы в то время как DashboardTOTPItem "абстрактная" модель отображения TOTPItem
+// тем не менее, логика чистая: TOTPItem просто знает про алгоритм и секрет
+// потом происходит from(TOTPItem) у DashboardTOTPItem и рождается код, оставшееся время
+// (то есть именно то что хотим показывать)
+// Ну а TOTPCellModel может что то добавить (title, subtitle и тд) или выкинуть для обертки в ячейку таблицы
+
 struct TOTPCellModel: Hashable {
     let id: String
     let title: String
